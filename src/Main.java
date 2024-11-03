f2525947a48445bbf45426930c3eba20badd0a05
abdoemad552 <abdoemad552@gmail.com>
2024-11-03 19:56:55 +0200

    Nearly completed scanner.
    In this commit, the following is done:
    	- Added Token class that represents a token.
    	- Added TokenPattern class which contains all tokens to be used.
    	- The input is taken character by character.
    	- To finish input:
    		- On windows: Press Ctrl + Z
    		- On Linux/MacOS: Press Ctrl + D

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,60 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.*;
 
 public class Main {
+    private static final String KEYWORD             = "keyword";
+    private static final String IDENTIFIER          = "identifier";
+    private static final String NUMERIC_LITERAL     = "numericLiteral";
+    private static final String STRING_LITERAL      = "stringLiteral";
+    private static final String CHARACTER_LITERAL   = "characterLiteral";
+    private static final String OPERATOR            = "operator";
+    private static final String SPECIAL_CHARACTER   = "specialCharacter";
+    private static final String WHITE_SPACE         = "whiteSpace";
+    private static final String UNKNOWN             = "unknown";
+
     public static void main(String[] args) {
-        System.out.println("Hello compiler!");
+        System.out.println("Enter code (press `Ctrl` + `D` (Linux/macOS) or `Ctrl` + `Z` (Windows) to finish input):");
+
+        StringBuilder inputCode = new StringBuilder();
+        try {
+            int inputChar;
+            while ((inputChar = System.in.read()) != -1) {
+                inputCode.append((char) inputChar);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Pattern pattern = TokenPattern.getAllInOnePattern();
+        Matcher matcher = pattern.matcher(inputCode.toString());
+
+        List<Token> tokens = new ArrayList<>();
+        while (matcher.find()) {
+            if (matcher.group(KEYWORD) != null) {
+                tokens.add(new Token(KEYWORD, matcher.group(KEYWORD)));
+            } else if (matcher.group(IDENTIFIER) != null) {
+                tokens.add(new Token(IDENTIFIER, matcher.group(IDENTIFIER)));
+            } else if (matcher.group(NUMERIC_LITERAL) != null) {
+                tokens.add(new Token(NUMERIC_LITERAL, matcher.group(NUMERIC_LITERAL)));
+            } else if (matcher.group(STRING_LITERAL) != null) {
+                tokens.add(new Token(STRING_LITERAL, matcher.group(STRING_LITERAL)));
+            } else if (matcher.group(CHARACTER_LITERAL) != null) {
+                tokens.add(new Token(CHARACTER_LITERAL, matcher.group(CHARACTER_LITERAL)));
+            } else if (matcher.group(OPERATOR) != null) {
+                tokens.add(new Token(OPERATOR, matcher.group(OPERATOR)));
+            } else if (matcher.group(SPECIAL_CHARACTER) != null) {
+                tokens.add(new Token(SPECIAL_CHARACTER, matcher.group(SPECIAL_CHARACTER)));
+            } else if (matcher.group(WHITE_SPACE) != null) {
+                tokens.add(new Token(WHITE_SPACE, matcher.group(WHITE_SPACE)));
+            } else if (matcher.group(UNKNOWN) != null) {
+                tokens.add(new Token(UNKNOWN, matcher.group(UNKNOWN)));
+            }
+        }
+
+        for (Token token : tokens) {
+            if (token.getName().equals(WHITE_SPACE)) continue;
+            System.out.println(token);
+        }
     }
 }
---
+++ src/TokenPattern.java
@@ -0,0 +1,62 @@
+import java.util.regex.Pattern;
+
+enum TokenPattern {
+    NOTHING(""),
+    KEYWORD(
+        "(?<keyword>\\b(void|char|short|int|long|float|double|const|if|else|switch|case|default|for|while|break|continue|return|sizeof|static|unsigned)\\b)"
+    ),
+    IDENTIFIER(
+        "(?<identifier>\\b([a-zA-Z_][a-zA-Z0-9_]*)\\b)"
+    ),
+    NUMERIC_LITERAL(
+        "(?<numericLiteral>\\b((\\d*\\.\\d+|\\d+\\.\\d*|\\d+)([Ee][+-]?\\d+)?)\\b)"
+    ),
+    STRING_LITERAL(
+        "(?<stringLiteral>\"([^\"\\\\]|\\\\[\\s\\S])*\")"
+    ),
+    CHARACTER_LITERAL(
+        "(?<characterLiteral>'([^'\\\\]|\\\\.)')"
+    ),
+    OPERATOR(
+        "(?<operator>&&|(\\|\\|)|(\\+\\+)|--|(<<|>>|[-+*/%=!<>&^|~])=?)"
+    ),
+    SPECIAL_CHARACTER(
+        "(?<specialCharacter>[;{}\\[\\](),.:])"
+    ),
+    WHITE_SPACE(
+        "(?<whiteSpace>\\s+)"
+    ),
+    UNKNOWN(
+        "(?<unknown>.+)"
+    );
+
+    private static final Pattern allInOnePattern;
+
+    private String pattern;
+
+    private TokenPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public static Pattern getAllInOnePattern() {
+        return allInOnePattern;
+    }
+
+    static {
+        allInOnePattern = Pattern.compile(String.join("|",
+            TokenPattern.KEYWORD            .getPattern(),
+            TokenPattern.IDENTIFIER         .getPattern(),
+            TokenPattern.NUMERIC_LITERAL    .getPattern(),
+            TokenPattern.STRING_LITERAL     .getPattern(),
+            TokenPattern.CHARACTER_LITERAL  .getPattern(),
+            TokenPattern.OPERATOR           .getPattern(),
+            TokenPattern.SPECIAL_CHARACTER  .getPattern(),
+            TokenPattern.WHITE_SPACE        .getPattern(),
+            TokenPattern.UNKNOWN            .getPattern()
+        ));
+    }
+}
